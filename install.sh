19d44eb6b8491afd3a5d275764c2aafcbe9aafda
Dominic Giglio <humanshell@gmail.com>
2011-10-15 09:59:56 -0400

    added linking code

--- install.sh
+++ install.sh
@@ -5,5 +5,16 @@
 # setup script variables
 OS=$(uname)
 
-# create soft links to config files
+# create soft link to rvm config
 ln -s $HOME/.dotfiles/rvmrc $HOME/.rvmrc
+
+# create soft links to vim config files
+ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+
+# create soft link to bash config
+if [ $OS == "Darwin" ]
+  ln -s $HOME/.dotfiles/bash_profile $HOME/.bash_profile
+else
+  ln -s $HOME/.dotfiles/bash_profile $HOME/.bashrc
+fi
+
