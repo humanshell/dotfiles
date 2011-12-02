a895aef716749c0633639a5c9920f0457e82cdd5
Dominic Giglio <humanshell@gmail.com>
2011-12-01 22:55:19 -0500

    added create autoload dir to install

--- install.sh
+++ install.sh
@@ -24,11 +24,12 @@ else
 fi
 
 # grab pathogen.vim from github so submodule plugins work
+mkdir ~/.dotfiles/vim/autoload
 if [ $OS == "Darwin" ]; then
   curl -so ~/.dotfiles/vim/autoload/pathogen.vim \
         https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 else
-  wget -O - ~/.dotfiles/vim/autoload/pathogen.vim \
+  wget -O ~/.dotfiles/vim/autoload/pathogen.vim \
         https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 fi
 
