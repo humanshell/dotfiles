00f580fc99ee79e942cb9fd0767e20363c89b458
Dominic Giglio <humanshell@gmail.com>
2012-03-12 01:21:53 -0400

    added general settings section to emacs init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -22,3 +22,85 @@
     (package-initialize)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; general settings
+;;
+(menu-bar-mode  t)                       ;; show the menu...
+(mouse-avoidance-mode 'jump)             ;; mouse ptr when cursor is too close
+(tool-bar-mode -1)                       ;; turn-off toolbar 
+
+(setq cua-enable-cua-keys nil)           ;; only for rectangles
+(cua-mode t)
+
+(setq ;; scrolling
+  scroll-margin 0                        ;; do smooth scrolling, ...
+  scroll-conservatively 100000           ;; ... the defaults ...
+  scroll-up-aggressively 0               ;; ... are very ...
+  scroll-down-aggressively 0             ;; ... annoying
+  scroll-preserve-screen-position t)     ;; preserve screen pos with C-v/M-v 
+
+(setq fringe-mode '(1 . 0))              ;; emacs 22+
+(delete-selection-mode 1)                ;; delete the sel with a keyp
+
+(setq x-select-enable-clipboard t        ;; copy-paste should work ...
+  interprogram-paste-function            ;; ...with...
+  'x-cut-buffer-or-selection-value)      ;; ...other X clients
+
+(setq search-highlight t                 ;; highlight when searching... 
+  query-replace-highlight t)             ;; ...and replacing
+(fset 'yes-or-no-p 'y-or-n-p)            ;; enable y/n answers to yes/no 
+
+(setq completion-ignore-case t           ;; ignore case when completing...
+  read-file-name-completion-ignore-case t) ;; ...filenames too
+
+(setq initial-scratch-message
+  ";; scratch buffer created -- happy hacking\n")
+
+(setq-default
+ frame-title-format
+ '(:eval
+   (format "%s@%s:%s"
+           (or (file-remote-p default-directory 'user) user-login-name)
+           (or (file-remote-p default-directory 'host) system-name)
+           (file-name-nondirectory (or (buffer-file-name) default-directory)))))
+
+(put 'narrow-to-region 'disabled nil)    ;; enable...
+(put 'erase-buffer 'disabled nil)        ;; ... useful things
+(file-name-shadow-mode t)                ;; be smart about filenames in mbuf
+
+(setq inhibit-startup-message t          ;; don't show ...    
+  inhibit-startup-echo-area-message t)   ;; ... startup messages
+(setq require-final-newline t)           ;; end files with a newline
+
+
+;; slick-copy: make copy-past a bit more intelligent
+;; from: http://www.emacswiki.org/emacs/SlickCopy
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single
+line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (message "Copied line")
+      (list (line-beginning-position)
+               (line-beginning-position 2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single
+line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (list (line-beginning-position)
+        (line-beginning-position 2)))))
+
+
+;; key board / input method settings
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-language-environment "UTF-8")       ; prefer utf-8 for language settings
+(set-input-method nil)                   ; no funky input for normal editing;
+(setq read-quoted-char-radix 10)         ; use decimal, not octal
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
