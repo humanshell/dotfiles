7310bd5e0b93a44cc65bcc8516fa0256286012f9
Dominic Giglio <humanshell@gmail.com>
2012-03-12 01:11:23 -0400

    added initial comments and load path sections to emacs init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,1 +1,24 @@
-;; emacs configuration
+;; Local Emacs config file
+;;
+;; Original code from:
+;; -*-mode: Emacs-Lisp; folding-mode:t-*-
+;; Copyright (C) 1996-2010  Dirk-Jan C. Binnema.
+;; URL: http://www.djcbsoftware.nl/dot-emacs.html
+;; This file is free software licensed under the terms of the
+;; GNU General Public License, version 3 or later.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; set the load path  
+
+;; add everything under ~/.emacs.d to it
+(let* ((my-lisp-dir "~/.emacs.d/")
+        (default-directory my-lisp-dir))
+  (setq load-path (cons my-lisp-dir load-path))
+  (normal-top-level-add-subdirs-to-load-path))
+
+(when (file-exists-p "~/.emacs.d/elpa/package.el")
+  (when (load (expand-file-name "~/.emacs.d/elpa/package.el"))
+    (package-initialize)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
