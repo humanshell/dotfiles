863fb31fc82da86da3cf5961ea4dcec7bab68987
Dominic Giglio <humanshell@gmail.com>
2012-04-06 19:06:43 -0400

    added logic to linking files and added rbenv/ruby-build install

--- install.sh
+++ install.sh
@@ -9,20 +9,20 @@ OS=$(uname)
 GIT=$(which git)
 
 # create soft links to config files
-ln -s $HOME/.dotfiles/htoprc $HOME/.htoprc
-ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
-ln -s $HOME/.dotfiles/gemrc $HOME/.gemrc
-ln -s $HOME/.dotfiles/ackrc $HOME/.ackrc
-ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
-ln -s $HOME/.dotfiles/vim $HOME/.vim
-ln -s $HOME/.dotfiles/rbenvrc $HOME/.rbenvrc
-ln -s $HOME/.dotfiles/phpenvrc $HOME/.phpenvrc
+[[ ! -h "$HOME/.htoprc" ]]   && ln -s $HOME/.dotfiles/htoprc $HOME/.htoprc
+[[ ! -h "$HOME/.gtconfig" ]] && ln -s $HOME/.dotfiles/gitconfig $HOME/.gitconfig
+[[ ! -h "$HOME/.gemrc" ]]    && ln -s $HOME/.dotfiles/gemrc $HOME/.gemrc
+[[ ! -h "$HOME/.ackrc" ]]    && ln -s $HOME/.dotfiles/ackrc $HOME/.ackrc
+[[ ! -h "$HOME/.vimrc" ]]    && ln -s $HOME/.dotfiles/vimrc $HOME/.vimrc
+[[ ! -h "$HOME/.rbenvrc" ]]  && ln -s $HOME/.dotfiles/rbenvrc $HOME/.rbenvrc
+[[ ! -h "$HOME/.phpenvrc" ]] && ln -s $HOME/.dotfiles/phpenvrc $HOME/.phpenvrc
+[[ ! -h "$HOME/.vim" ]]      && ln -s $HOME/.dotfiles/vim $HOME/.vim
 
 # create soft link to bash_config
-if [ $OS == "Darwin" || $OS == "FreeBSD" ]; then
-  [[ ! -f "$HOME/.bash_profile" ]] && ln -s $HOME/.dotfiles/bash_config $HOME/.bash_profile
+if [[ $OS == "Darwin" || $OS == "FreeBSD" ]]; then
+  [[ ! -h "$HOME/.bash_profile" ]] && ln -s $HOME/.dotfiles/bash_config $HOME/.bash_profile
 else
-  [[ ! -f "$HOME/.bashrc" ]] && ln -s $HOME/.dotfiles/bash_config $HOME/.bashrc
+  [[ ! -h "$HOME/.bashrc" ]] && ln -s $HOME/.dotfiles/bash_config $HOME/.bashrc
 fi
 
 # create soft links for mysql and sqlite histories to /dev/null
@@ -30,7 +30,7 @@ fi
 [[ ! -h "$HOME/.sqlite_history" ]] && ln -s /dev/null $HOME/.sqlite_history
 
 # grab pathogen.vim from github so submodule plugins work
-if [ $OS == "Darwin" ]; then
+if [[ $OS == "Darwin" ]]; then
   curl -so ~/.dotfiles/vim/autoload/pathogen.vim \
         https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 else
@@ -40,4 +40,11 @@ fi
 
 # initialize and update submodules
 $GIT submodule init && $GIT submodule update
+
+# install rbenv and ruby-build
+if [[ ! -d $HOME/.rbenv ]]; then
+  $GIT clone git://github.com/sstephenson/rbenv.git $HOME/.rbenv
+  mkdir -p $HOME/.rbenv/plugins
+  $GIT clone git://github.com/sstephenson/ruby-build.git $HOME/.rbenv/plugins/ruby-build
+fi
 
